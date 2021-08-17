51ca9068aa4d3225fa02c7c802435f07dc8aabdd
정석우 <ggp03016@naver.com>
2021-08-17 14:42:53 +0900

    회원도메인 개발(Repository,Service, ServiceTest)

--- src/main/java/jpabook/jpashop/repository/CategoryRepository.java
+++ src/main/java/jpabook/jpashop/repository/CategoryRepository.java
@@ -1,7 +1,6 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Category;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
